36eca1df5b8748defe90bd78d3b08bedf560e46b
“Farmersteve121” <5115983@qq.com>
2026-09-05 22:27:51 +0800

    包含后端代码

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/English-Website/'   // 关键：设置资源基础路径
 })
